21e60f2d40a6a83d374a647fa1fa55effe1cf095
unknown <tecmatheusg@hotmail.com>
2024-06-02 19:21:44 -0300

    add conteudo dinamico scroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, } from '@angular/core';
 import { GamesApiService } from '../services/games-api.service';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { IonContent } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   // styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
+
+  @ViewChild('scrollableBemAvaliados') scrollableBemAvaliados!: ElementRef;
+  @ViewChild('scrollableIndie') scrollableIndie!: ElementRef;
+  @ViewChild('scrollableRpg') scrollableRpg!: ElementRef;
 
   carouselInnerHtml: any;
   lancamentos: any;
@@ -16,6 +22,69 @@ export class HomePage implements OnInit {
   jogosRpg: any;
   jogosIndie: any;
   constructor(public gamesApiService: GamesApiService, private router: Router) { }
+
+  ngAfterViewInit() {
+    this.HandleScrollBemAvaliados();
+    this.HandleScrollJogosIndie();
+    this.HandleScrollJogosRpg();
+  }
+
+  HandleScrollBemAvaliados(){
+    sessionStorage.setItem('bem-avaliados-pagina', '2');
+
+    let paginaBemAvaliados = Number(sessionStorage.getItem('bem-avaliados-pagina'));
+
+    this.scrollableBemAvaliados.nativeElement.addEventListener('scroll', (event: CustomEvent) => {
+      let element = this.scrollableBemAvaliados.nativeElement;
+      let fimScroll = element.scrollWidth - element.scrollLeft === element.clientWidth;
+      //let momentoLoad = element.clientWidth * 2;
+
+      if (fimScroll) {
+        console.log(element.scrollWidth - element.scrollLeft)
+        this.LoadMoreBemAvaliados(paginaBemAvaliados);
+        sessionStorage.setItem('bem-avaliados-pagina', paginaBemAvaliados.toString());
+        paginaBemAvaliados++;        
+      }  
+    });
+  }
+
+  HandleScrollJogosIndie(){
+    sessionStorage.setItem('indie-pagina', '2');
+
+    let paginaIndie = Number(sessionStorage.getItem('indie-pagina'));
+
+    this.scrollableIndie.nativeElement.addEventListener('scroll', (event: CustomEvent) => {
+      let element = this.scrollableIndie.nativeElement;
+      let fimScroll = element.scrollWidth - element.scrollLeft === element.clientWidth;
+      //let momentoLoad = element.clientWidth * 2;
+
+      if (fimScroll) {
+        console.log(element.scrollWidth - element.scrollLeft)
+        this.LoadMoreIndie(paginaIndie);
+        sessionStorage.setItem('indie-pagina', paginaIndie.toString());
+        paginaIndie++;        
+      }  
+    });
+  }
+
+  HandleScrollJogosRpg(){
+    sessionStorage.setItem('rpg-pagina', '2');
+
+    let paginaRpg = Number(sessionStorage.getItem('rpg-pagina'));
+
+    this.scrollableRpg.nativeElement.addEventListener('scroll', (event: CustomEvent) => {
+      let element = this.scrollableRpg.nativeElement;
+      let fimScroll = element.scrollWidth - element.scrollLeft === element.clientWidth;
+      //let momentoLoad = element.clientWidth * 2;
+      console.log('rolando scroll rpg')
+      if (fimScroll) {
+        console.log(element.scrollWidth - element.scrollLeft)
+        this.LoadMoreRpg(paginaRpg);
+        sessionStorage.setItem('rpg-pagina', paginaRpg.toString());
+        paginaRpg++;        
+      }  
+    });
+  }
 
   ngOnInit() {
     this.gamesApiService.getJogos(1, "-rating, released").subscribe(
@@ -35,10 +104,35 @@ export class HomePage implements OnInit {
       })
   }
 
+  LoadMoreBemAvaliados(pagina: number) {
+    this.gamesApiService.getJogos(pagina, "-rating, released").subscribe(
+      (result: any) => {
+        let resultados = result?.results
+        resultados.forEach((jogo: any) => {
+          this.bemAvaliados.results.push(jogo);
+        });
+      })
+  }
 
-  // goToDetails(id: number) {
-  //   this.navCtrl.navigateForward(`/detalhes/${id}`);
-  // }
+  LoadMoreIndie(pagina: number) {
+    this.gamesApiService.getJogosGenero("indie", pagina).subscribe(
+      (result: any) => {
+        let resultados = result?.results
+        resultados.forEach((jogo: any) => {
+          this.jogosIndie.results.push(jogo);
+        });
+      })
+  }
+
+  LoadMoreRpg(pagina: number) {
+    this.gamesApiService.getJogosGenero("role-playing-games-rpg", pagina).subscribe(
+      (result: any) => {
+        let resultados = result?.results
+        resultados.forEach((jogo: any) => {
+          this.jogosRpg.results.push(jogo);
+        });
+      })
+  }
 
   goToDetails(id: number) {
     this.router.navigate(['/detalhes', id])
