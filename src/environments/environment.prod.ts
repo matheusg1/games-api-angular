7c0f4b38a04e972705512ffd8e833fb7b7c5b1f8
unknown <tecmatheusg@hotmail.com>
2024-06-08 21:06:39 -0300

    ajustes variaveis de ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyArV96xm9rLUQDwkQrjkmH9xNC4_8bVktE",
+  authDomain: "games-app-58b2a.firebaseapp.com",
+  projectId: "games-app-58b2a",
+  storageBucket: "games-app-58b2a.appspot.com",
+  messagingSenderId: "874150140716",
+  appId: "1:874150140716:web:c0266862d31deae9b56a89",
+  measurementId: "G-CDMBBDN0MH"
 };
